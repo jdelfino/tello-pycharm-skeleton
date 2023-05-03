26cbd1603a8d07f4a595ce811acbc0f81a9f79fe
Joe Delfino <jdelfino@gmail.com>
2023-05-03 13:52:45 -0400

    example code for leds

--- drone.py
+++ drone.py
@@ -4,32 +4,25 @@ from tello_asyncio import Tello, MissionPadDetection, Vector
 tello_asyncio.DEFAULT_RESPONSE_TIMEOUT=30
 
 async def main():
-    drone = Tello(on_state=on_drone_state)
+    drone = Tello()
     try:
         await drone.connect()
-        await drone.takeoff()
-        await drone.enable_mission_pads()
-        await drone.set_mission_pad_detection(MissionPadDetection.DOWN)
 
-        await drone.move_up(50)
-        await drone.move_forward(100)
-        await asyncio.sleep(5) # Pauses for 5 seconds
-        await drone.go_to(Vector(0, 0, 40), 20, 4)
-        await asyncio.sleep(5)
+        # Command syntax reference here: https://dl.djicdn.com/downloads/RoboMaster+TT/Tello_SDK_3.0_User_Guide_en.pdf
+        # "mled s [color: r or b or p] [letter]" -> display the given letter in the given color (red/blue/purple)
+        # example: This line shows the letter "q" in blue
+        await drone.send_ext_command("mled s b q")
 
-        await drone.land()
+        # "mled g [sequence of 0/r/b/p]" -> set each pixel of the display to off (0), red (r), blue (b) or purple (p)
+        # example: This sets the screen to have 1 blank line, 1 red line, 1 blue line, 2 purple lines, 1 blue line, 1 red line, and one blank line
+        await drone.send_ext_command("mled g 00000000rrrrrrrrbbbbbbbbppppppppppppppppbbbbbbbbrrrrrrrr00000000")
+
+        # "mled [scroll direction: l or r or u or d] [color: r or b or p] [speed: 0.1 - 2.5] [words]" ->
+        #    display the words shown, and scroll them in the given direction with the given speed
+        # example: This shows the words "hi innovation lab!" in red, and they scroll up quickly
+        await drone.send_ext_command("mled u r 2.5 hi innovation lab!")
+        await asyncio.sleep(10)
     finally:
         await drone.disconnect()
-
-def on_drone_state(drone, state):
-    if state.mission_pad == -1:
-        print(f"(b: {state.battery}%) I don't see a mission pad")
-    else:
-        print(f"(b: {state.battery}%) I see mission pad: {state.mission_pad}, " +
-            f"I'm in position: (x: {state.mission_pad_position.x}, " +
-            f"y: {state.mission_pad_position.y}, z:{state.mission_pad_position.z})")
-
-    drone.mission_pad = state.mission_pad
-    drone.mission_pad_position = state.mission_pad_position
 
 asyncio.run(main())
