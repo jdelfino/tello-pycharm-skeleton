965a8a98586f6f02f71bc564c87a79f0f1c37d72
Joe Delfino <jdelfino@gmail.com>
2023-11-29 10:56:02 -0500

    add example of scrolling image

--- drone.py
+++ drone.py
@@ -9,7 +9,8 @@ async def main():
         await drone.connect()
 
         # Command syntax reference here: https://dl.djicdn.com/downloads/RoboMaster+TT/Tello_SDK_3.0_User_Guide_en.pdf
-        # "mled s [color: r or b or p] [letter]" -> display the given letter in the given color (red/blue/purple)
+        # "mled s [color: r or b or p] [letter]" ->
+        #    display the given letter in the given color (red/blue/purple)
         # example: This line shows the letter "q" in blue
         await drone.send_ext_command("mled s b q")
         await asyncio.sleep(5)
@@ -20,13 +21,16 @@ async def main():
         await drone.send_ext_command("mled l r 2.5 hi innovation lab!")
         await asyncio.sleep(5)
 
-        # "mled g [sequence of 0/r/b/p]" -> set each pixel of the display to off (0), red (r), blue (b) or purple (p)
-        # example: This sets the screen to have 1 blank line, 1 red line, 1 blue line, 2 purple lines, 1 blue line, 1 red line, and one blank line
+        # "mled g [sequence of 0/r/b/p]" ->
+        #    set each pixel of the display to off (0), red (r), blue (b) or purple (p)
+        # example: This sets the screen to have 1 blank line, 1 red line, 1 blue line, 2 purple lines,
+        #    1 blue line, 1 red line, and one blank line
         await drone.send_ext_command("mled g 00000000rrrrrrrrbbbbbbbbppppppppppppppppbbbbbbbbrrrrrrrr00000000")
         await asyncio.sleep(5)
 
         # "mled [scroll direction: l or r or u or d] g [speed: 0.1 - 2.5] [sequence of 0/r/b/p]" ->
-        #    displays a design of pixels, similar to the previous example, but also scrolls it in the given direction
+        #    displays a design of pixels, similar to the previous example, but also scrolls it in the
+        #    given direction
         # example: This shows the same pattern as the previous example, but scrolls it down slowly
         await drone.send_ext_command("mled d g 0.5 00000000rrrrrrrrbbbbbbbbppppppppppppppppbbbbbbbbrrrrrrrr00000000")
         await asyncio.sleep(5)
